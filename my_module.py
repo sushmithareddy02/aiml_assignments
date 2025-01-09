7e5f5c1bdf8ba5a3781ea28d5a467304a21522a1
sushmithareddy02 <reddysushmitha909@gmail.com>
2025-01-09 13:19:09 +0530

    Add files via upload

---
+++ my_module.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+greet = lambda name : print(f"Good Morning {name}!")
+greet("sushmitha")
+
+
+# In[2]:
+
+
+product = lambda a,b,c : a*b*c
+product(3,4,1)
+
+
+# In[3]:
+
+
+even = lambda L : [x for x in L if x%2 == 0]
+my_list = [100,30,40,55,60]
+even(my_list)
+
+
+# In[ ]:
+
+
+
+
