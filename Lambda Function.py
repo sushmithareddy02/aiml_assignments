c1f89a48512ae24af9c573e66e4c3b699636f698
sushmithareddy02 <reddysushmitha909@gmail.com>
2025-01-24 22:51:40 +0530

    Add files via upload

---
+++ Lambda Function.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #Lambda Function
+# 
+# 
+
+# In[1]:
+
+
+greet = lambda name : print(f"Good Morning {name}!")
+greet("sushmitha")
+
+
+# In[5]:
+
+
+product = lambda a,b,c : a*b*c
+product(3,4,1)
+
+
+# In[8]:
+
+
+even = lambda L : [x for x in L if x%2 == 0]
+my_list = [100,30,40,55,60]
+even(my_list)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
